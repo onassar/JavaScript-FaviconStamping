2324723938b7d6ef3c70e8bf331b4c8d5af38075
Oliver Nassar <oliver.nassar@namecheap.com>
2022-06-01 17:29:17 -0400

    Reorg to support multiple provders and overriding of speciif c hosts to a provider

--- FaviconStamping.js
+++ FaviconStamping.js
@@ -8,7 +8,7 @@
  * @see     https://unavatar.io/
  * @see     https://css-tricks.com/favicons-next-to-external-links/
  * @see     https://www.google.com/s2/u/0/favicons?domain=css-tricks.com
- * @todo    Add ability to choose between favicon vendors
+ * @todo    Add ability to choose between favicon providers
  * @todo    Add DOM change event listeners to handle dynamically added elements
  * @access  public
  * @var     Object
@@ -29,17 +29,48 @@ var FaviconStamping = (function() {
     var __config = {
 
         /**
-         * hostnameMapping
+         * defaultProviderKey
+         * 
+         * @access  private
+         * @var     String (default: 'duckDuckGo')
+         */
+        defaultProviderKey: 'duckDuckGo',
+        // defaultProviderKey: 'google',
+
+        /**
+         * hostnameOverrideMapping
+         * 
+         * Stores a hash which is used to determine whether specific hostnames
+         * should have their hostname swapped out when requesting a favicon.
+         * This is useful for hostnames which may not have favicon settings
+         * configured, but are naturally associated with another hostname.
          * 
          * @access  private
          * @var     Object
          */
-        hostnameMapping: {
+        hostnameOverrideMapping: {
             'dash.cloudflare.com': 'cloudflare.com'
         },
 
         /**
+         * hostnameProviderMapping
+         * 
+         * Stores a hash which is used to determine whether specific hostnames
+         * should have their provider overridden (likely because one provider
+         * doesn't return a good icon for specific hostnames).
+         * 
+         * @access  private
+         * @var     Object
+         */
+        hostnameProviderMapping: {
+            'canva.com': 'google',
+            'www.canva.com': 'google'
+        },
+
+        /**
          * selectors
+         * 
+         * The selectors to stamp with a favicon.
          * 
          * @access  private
          * @var     Array
@@ -72,48 +103,18 @@ var FaviconStamping = (function() {
      * 
      */
 
-    // /**
-    //  * __get1PasswordFaviconURL
-    //  * 
-    //  * @access  private
-    //  * @param   HTMLElement $element
-    //  * @return  String
-    //  */
-    // var __get1PasswordFaviconURL = function($element) {
-    //     var href = $element.getAttribute('href'),
-    //         url = new URL(href),
-    //         hostname = url.hostname,
-    //         faviconURL = 'https://c.1password.com/richicons/images/login/120/' + (hostname) + '.png';
-    //     return faviconURL;
-    // };
-
     /**
-     * __getDuckDuckGoFaviconURL
+     * __getFaviconURL
      * 
      * @access  private
      * @param   HTMLElement $element
      * @return  String
      */
-    var __getDuckDuckGoFaviconURL = function($element) {
-        var hostname = __getHostname($element),
-            faviconURL = 'https://icons.duckduckgo.com/ip3/' + (hostname) + '.ico';
+    var __getFaviconURL = function($element) {
+        var providerFunction = __getProviderFunction($element),
+            faviconURL = providerFunction($element);
         return faviconURL;
     };
-
-    // /**
-    //  * __getGoogleFaviconURL
-    //  * 
-    //  * @access  private
-    //  * @param   HTMLElement $element
-    //  * @return  String
-    //  */
-    // var __getGoogleFaviconURL = function($element) {
-    //     var href = $element.getAttribute('href'),
-    //         url = new URL(href),
-    //         hostname = url.hostname,
-    //         faviconURL = 'https://unavatar.io/' + (hostname) + '.ico';
-    //     return faviconURL;
-    // };
 
     /**
      * __getHostname
@@ -126,8 +127,23 @@ var FaviconStamping = (function() {
         var href = $element.getAttribute('href'),
             url = new URL(href),
             hostname = url.hostname;
-        hostname = __config.hostnameMapping[hostname] || hostname;
+        hostname = __config.hostnameOverrideMapping[hostname] || hostname;
         return hostname;
+    };
+
+    /**
+     * __getProviderFunction
+     * 
+     * @access  private
+     * @param   HTMLElement $element
+     * @return  Function
+     */
+    var __getProviderFunction = function($element) {
+        var defaultProviderKey = __config.defaultProviderKey,
+            hostname = __getHostname($element),
+            providerKey = __config.hostnameProviderMapping[hostname] || defaultProviderKey,
+            fn = __providers[providerKey];
+        return fn;
     };
 
     /**
@@ -142,21 +158,6 @@ var FaviconStamping = (function() {
         return selector;
     };
 
-    // /**
-    //  * __getUnavatarFaviconURL
-    //  * 
-    //  * @access  private
-    //  * @param   HTMLElement $element
-    //  * @return  String
-    //  */
-    // var __getUnavatarFaviconURL = function($element) {
-    //     var href = $element.getAttribute('href'),
-    //         url = new URL(href),
-    //         hostname = url.hostname,
-    //         faviconURL = 'https://unavatar.io/' + (hostname) + '.ico';
-    //     return faviconURL;
-    // };
-
     /**
      * __includeFavicon
      * 
@@ -165,7 +166,7 @@ var FaviconStamping = (function() {
      * @return  void
      */
     var __includeFavicon = function($element) {
-        var faviconURL = __getDuckDuckGoFaviconURL($element);
+        var faviconURL = __getFaviconURL($element);
         $element.style.backgroundImage = 'url("' + (faviconURL) + '")';
         $element.style.backgroundRepeat = 'no-repeat';
         $element.style.paddingLeft = __config.styles.paddingLeft;
@@ -173,6 +174,75 @@ var FaviconStamping = (function() {
         $element.style.backgroundSize = 'auto 75%';
         $element.style.backgroundPosition = 'left center';
 
+    };
+
+    /**
+     * __providers
+     * 
+     * @access  private
+     * @var     Object
+     */
+    var __providers = {
+
+        /**
+         * 1Password
+         * 
+         * @todo    Implement
+         * @access  private
+         * @param   HTMLElement $element
+         * @return  String
+         */
+        '1Password': function($element) {
+            throw new Error('Implement');
+            // var href = $element.getAttribute('href'),
+            //     url = new URL(href),
+            //     hostname = url.hostname,
+            //     faviconURL = 'https://c.1password.com/richicons/images/login/120/' + (hostname) + '.png';
+            // return faviconURL;
+        },
+
+        /**
+         * duckDuckGo
+         * 
+         * @access  private
+         * @param   HTMLElement $element
+         * @return  String
+         */
+        duckDuckGo: function($element) {
+            var hostname = __getHostname($element),
+                faviconURL = 'https://icons.duckduckgo.com/ip3/' + (hostname) + '.ico';
+            return faviconURL;
+        },
+
+        /**
+         * google
+         * 
+         * @access  private
+         * @param   HTMLElement $element
+         * @return  String
+         */
+        google: function($element) {
+            var hostname = __getHostname($element),
+                faviconURL = 'https://www.google.com/s2/favicons?domain=' + (hostname);
+            return faviconURL;
+        },
+
+        /**
+         * __getUnavatarFaviconURL
+         * 
+         * @todo    Implement
+         * @access  private
+         * @param   HTMLElement $element
+         * @return  String
+         */
+        unavatar: function($element) {
+            throw new Error('Implement');
+            // var href = $element.getAttribute('href'),
+            //     url = new URL(href),
+            //     hostname = url.hostname,
+            //     faviconURL = 'https://unavatar.io/' + (hostname) + '.ico';
+            // return faviconURL;
+        }
     };
 
     /**
